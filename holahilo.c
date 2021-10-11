3b0aa37000dd4b725b6704de85160e0234813301
Miguel Palomera <miguel.palomera@gmail.com>
2021-10-11 23:59:24 +0000

    practicas

---
+++ holahilo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define NUM_THREADS 10000
+
+int saldo;
+pthread_mutex_t saldoLock = PTHREAD_MUTEX_INITIALIZER;
+
+void * suma100(void *arg){
+    int localsaldo;
+    pthread_mutex_lock(&saldoLock);
+    localsaldo = saldo;
+    localsaldo += 100;
+    saldo = localsaldo; 
+    pthread_mutex_unlock(&saldoLock);
+    pthread_exit(NULL);
+}
+
+
+int main(){
+    pthread_t threads[NUM_THREADS];
+    saldo = 0;
+    for(int i = 0; i < NUM_THREADS; i++){
+        pthread_create(&threads[i],NULL,suma100,NULL);
+    }
+
+    for(int i = 0; i < NUM_THREADS; i++){
+        pthread_join(threads[i],NULL);
+    }
+    printf("Saldo final %d\n",saldo);
+    
+    pthread_exit(NULL);
+
+}
